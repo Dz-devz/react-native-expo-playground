78ac8e0456c33b13cefa696171922dc4addb6cbd
Dz-devz <darwinbjordan@gmail.com>
2024-07-25 19:57:57 +0800

    progress: learned Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 
 const RootLayout = () => {
-  return <Slot />
+  return (
+  <Stack>
+    <Stack.Screen name="index" options={{headerShown: false}} />
+  </Stack>
+  )
 }
 
 export default RootLayout;
