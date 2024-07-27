54fb270e38ddad3d2161861d708cd2a49f6d6329
Dz.dev <darwinbjordan@gmail.com>
2024-07-27 12:59:50 +0800

    progress: added nativewind for css and adeed app.d.ts to avoid classname type error

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{color: 'blue'}} >
-      Go to Profile
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
