7cf1630c8909366b723e8d8448152ee04705c67a
Dz.dev <darwinbjordan@gmail.com>
2024-07-28 10:42:50 +0800

    progress: added logo and use safeareaview and scrollview

--- constants/images.js
+++ constants/images.js
@@ -1,9 +1,7 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
-import cards from "../assets/images/cards.png";
-import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/logo-small.png";
+import DzLogo from "../assets/images/DzLogo.png";
+import DzLogoNoDz from "../assets/images/DzLogoNoDz.png";
 import empty from "../assets/images/empty.png";
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.jpg";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default { profile, thumbnail, DzLogoNoDz, DzLogo, empty };
