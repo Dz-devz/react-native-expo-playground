d75dbfb49191ced02a1e8540bd0262d0f49955d7
Dz-devz <darwinbjordan@gmail.com>
2024-07-29 21:51:15 +0800

    progress: set up and design sign-in.tsx

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {images} from "../../constants"
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>Sign In</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image source={images.DzLogo} resizeMode="contain" className="w-[115px] h-[35px]" />
+          <Text className="text-2xl text-white text-semibold font-psemibold mt-10" >Log In to DzPen Music</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
